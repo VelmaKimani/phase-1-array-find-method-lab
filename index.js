9479f2b7b336632d1a8703b58c15f05cc2e9d1f5
Velma Kimani <stanvelma16@gmail.com>
2023-09-26 10:56:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin(record){
+    for(let win of record){
+      if(win.result === "W"){
+        return win.year;
+      }
+      else if(win.result === "L"){
+        return win.year;
+      }
+    }
+    // if(record.year === 2015){
+    //     return record;
+    // } 
+}
+record.find(superbowlWin);
